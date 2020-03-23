f14ba24c66b881e1bb463252c18b742067281981
unknown <pierrick.durrmeyer@gmail.com>
2020-03-23 15:44:02 +0100

    Premier commit

---
+++ WebApplication_Module6_TP1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_Module6_TP1.MvcApplication" Language="C#" %>
---
+++ WebApplication_Module6_TP1/Models/BO/Arme.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication_Module6_TP1.Models.BO
+{
+    public class Arme
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int Degats { get; set; }
+    }
+}
